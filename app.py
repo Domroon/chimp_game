e601d9a1d7e8afc6f8dddf07656a819816efc881
Dominik Haeusser <haeusser-dominik@web.de>
2020-11-26 22:13:05 +0100

    add Chimp class

--- app.py
+++ app.py
@@ -59,3 +59,68 @@ class Fist(pg.sprite.Sprite):
     def unpunch(self):
         """called to pull the fist back"""
         self.punching = 0
+
+
+class Chimp(pg.sprite.Sprite):
+    """moves a monkey critter across the screen. It can spin the
+       monkey when it is punched"""
+    def __init__(self):
+        pg.sprite.Sprite.__init__(self)  # call the Sprite initializer
+        self.image, self.rect = load_image('chimp.jpg', -1)
+        screen = pg.display.get_surface()
+        self.area = screen.get_rect()
+        self.rect.topleft = 10, 10
+        self.move = 9
+        self.dizzy = 0
+
+    def update(self):
+        """walk or spin, depending on the monkeys state"""
+        if self.dizzy:
+            self._spin()
+        else:
+            self._walk()
+
+    def _walk(self):
+        """move the monkey across the screen, and turn at the ends"""
+        newpos = self.rect.move((self.move, 0))
+        if not self.area.contains(newpos):
+            if self.rect.left < self.area.left or \
+                    self.rect.right > self.area.right:
+                self.move = -self.move
+                newpos = self.rect.move((self.move, 0))
+                self.image = pg.transform.flip(self.image, True, False)
+            self.rect = newpos
+
+    def _spin(self):
+        """spin the monkey image"""
+        center = self.rect.center
+        self.dizzy += 12
+        if self.dizzy >= 360:
+            self.dizzy = 0
+            self.image = self.original
+        else:
+            rotate = pg.transform.rotate
+            self.image = rotate(self.original, self.dizzy)
+        self.rect = self.image.get_rect(center=center)
+
+    def punched(self):
+        """this will cause the monkey to start spinning"""
+        if not self.dizzy:
+            self.dizzy = 1
+            self.original = self.image
+
+
+def main():
+    """this function is called when the program starts.
+       it initializes everything it needs, then runs in
+       a loop until the function returns."""
+    # Initialize Everything
+    pg.init()
+    screen = pg.display.set_mode((468, 60))
+    pg.display.set_caption('Monkey Fever')
+    pg.mouse.set_visible(0)
+
+
+# this calls the 'main' function when this script is executed
+if __name__ == "__main__":
+    main()
